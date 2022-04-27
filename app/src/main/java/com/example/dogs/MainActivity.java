692b87e3fd45923a4dfb6d766818ce122893ca8b
Yuriiru <panda-r@inbox.ru>
2022-04-27 12:58:47 +0300

    UREC INIT

--- app/src/main/java/com/example/dogs/MainActivity.java
+++ app/src/main/java/com/example/dogs/MainActivity.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
+
     int dog;
     String selectedItem;
     ArrayList<String> lines = new ArrayList<String>();
